7851351c0c0bcc6597b564e799d8fe7361008d81
Chaz Clark <chazclark@aviana.student.iastate.edu>
2017-02-24 20:15:22 -0600

    Initial Commit

---
+++ Rush_Buddies/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Rush_Buddies
+//
+//  Created by Chaz Clark on 2/24/17.
+//  Copyright © 2017 Rush Hour 4. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
